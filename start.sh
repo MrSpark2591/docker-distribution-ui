7c206c057f6b6e554326252b917cda3a5288a0b4
Dhaval chaudhary <mr.spark2591@gmail.com>
2017-04-12 19:52:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,6 +10,14 @@ die() {
 [[ -z "$ENV_REGISTRY_PROXY_FQDN" ]] && ENV_REGISTRY_PROXY_FQDN=$ENV_DOCKER_REGISTRY_HOST
 [[ -z "$ENV_REGISTRY_PROXY_PORT" ]] && ENV_REGISTRY_PROXY_PORT=$ENV_DOCKER_REGISTRY_PORT
 
+needModSsl=0
+if [ -n "$ENV_DOCKER_REGISTRY_USE_SSL" ]; then
+  echo "export DOCKER_REGISTRY_SCHEME=https" >> /etc/apache2/envvars
+  needModSsl=1
+else
+  echo "export DOCKER_REGISTRY_SCHEME=http" >> /etc/apache2/envvars
+fi
+
 echo $ENV_DOCKER_REGISTRY_HOST
 
 sed -i "s/REGISTRY_PORT/$ENV_DOCKER_REGISTRY_PORT/" /etc/nginx/conf.d/default.conf
